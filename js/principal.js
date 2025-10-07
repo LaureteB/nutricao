4fbf86fa4af49991c83d94183f05285b509a2980
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2025-10-07 09:31:36 -0300

    fgd

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,7 @@
             var titulo = document.querySelector("h1");
             console.log(titulo);
             console.log(titulo.textContent);
-            titulo.textContent = ("LAuraba");
+            titulo.textContent = ("Laura nutricão");
 
             var paciente = document.querySelector("#primeiro-paciente");
             console.log(paciente);
