b6a31a853e79bd7c5e8c6498821a0d466aa31475
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2025-10-28 10:06:43 -0300

    wef

--- js/principal.js
+++ js/principal.js
@@ -46,3 +46,15 @@ function mostraMensagem(){
 
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+    //alert("Oi eu sou o botão e fui clicado");
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+});
