d4d7be4cb678c8e3fb8be0e72e3b5008e09d33ab
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2025-10-31 10:04:24 -0300

    gu

--- js/principal.js
+++ js/principal.js
@@ -56,5 +56,25 @@ botaoAdicionar.addEventListener("click",function(event){
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
     
+    var tabela = document.querySelector("tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 });
