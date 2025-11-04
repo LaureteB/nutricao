1f610e2e3ed343d46de81f865c9541e845d37083
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2025-11-04 09:35:06 -0300

    wer

---
+++ js/calcula-imc.js
@@ -0,0 +1,47 @@
+
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("NUTRA-SE COM LAURA BOEING");
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+
+    if (peso <= 0 || peso >= 800) {
+        console.log("peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Invalido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+    titulo.addEventListener("click", mostraMensagem);
+    function mostraMensagem() {
+        alert("Olá eu fui clicado")
+    }
+
+}
